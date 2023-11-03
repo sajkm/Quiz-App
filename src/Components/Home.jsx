bdc7c2fdfced3ab100fc7ef934b9ea672683b05a
sajkm <sajkm321@gmail.com>
2023-11-03 21:20:02 +0530

    quiz app

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <div className='d-flex align-items-center justify-content-center bg-grey flex-column' style={{width:'100%', height:'100vh'}}>
+        <h1 className='fw-bolder'>QUIZ APP</h1>
+        <div className='container w-50 h-75 shadow border rounded justify-content-center d-flex'>
+           <div className='shadow alig mt-3 border rounded' style={{width:'50rem',height:'100px'}}>
+
+           </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
